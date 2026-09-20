0230cdb8adcedec3851c8babc0b34588af101b31
mohamedwaheedmohamed544-droid <mohamed.waheed.mohamed544@gmail.com>
2026-09-20 15:46:44 +0300

    Delete lib directory

--- lib/guard.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-import { redirect } from "next/navigation";
-import { currentUser } from "./auth";
-
-export async function guard() {
-  const user = await currentUser();
-  if (!user) redirect("/admin/login");
-  return user;
-}
