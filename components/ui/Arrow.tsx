2008f3f3142040dc5097ddbe73e3a4af6bddece2
mohamedwaheedmohamed544-droid <mohamed.waheed.mohamed544@gmail.com>
2026-09-20 14:57:26 +0300

    Delete components directory

--- components/ui/Arrow.tsx
+++
@@ -1,7 +0,0 @@
-export default function Arrow({ className = "arrow" }: { className?: string }) {
-  return (
-    <svg className={className} width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-      <path d="M4 12h15m0 0-6-6m6 6-6 6" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  );
-}
